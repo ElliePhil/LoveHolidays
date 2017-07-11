2b43d6f9ad84994bee41b6ac52cf959aeecac46d
ElliePhil <Elizabeth.Philip1@gmail.com>
2017-07-11 09:45:36 +0100

    Update MMBRun.java

--- src/test/java/MMBRun.java
+++ src/test/java/MMBRun.java
@@ -6,7 +6,7 @@ import org.junit.runner.RunWith;
  * Created by elizabeth on 09/07/2017.
  */
 @RunWith(Cucumber.class)
-@CucumberOptions(tags = "@MMB,@Scenario2",format = { "pretty","html:target/Rules/cucumber","json:target/Rules/cucumber.json"})
+@CucumberOptions(tags = "@MMB",format = { "pretty","html:target/Rules/cucumber","json:target/Rules/cucumber.json"})
 public class MMBRun {
 //Test needs to be run with Firefox v45 or v47
 
